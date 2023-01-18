1866c0b94c318a9c71d18f517ad8605f23d29d46
solomon244 <slegesse2444@gmail.com>
2023-01-18 22:00:45 +0300

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''the Redis NoSQL data storage.
+'''
+import uuid
+import redis
+from functools import wraps
+from typing import Any, Callable, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    '''Tracks the number of calls made to a method in a Cache class.
+    '''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''Invokes the given method after incrementing its call counter.
+        '''
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
