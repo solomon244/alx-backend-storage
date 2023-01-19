047644bab2e536948e08a09a1a6e156c3fd4839e
solomon244 <slegesse2444@gmail.com>
2023-01-19 05:35:30 +0300

    redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,26 +12,26 @@ redis_store = redis.Redis()
 '''
 
 
-def data_cacher(func: Callable) -> Callable:
+def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
     '''
-    @wraps(func)
+    @wraps(method)
     def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
         redis_store.incr(f'count:{url}')
-        n = redis_store.get(f'n:{url}')
-        if n:
-            return n.decode('utf-8')
-        n = func(url)
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
         redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'n:{url}', 10, n)
-        return n
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
     return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''track how many times a particular URL was accessed in the key "count:{url}" and cache the result with an expiration time of 10 seconds.
+    ''' track how many times a particular URL was accessed in the key "count:{url}" and cache the result with an expiration time of 10 seconds.
     '''
     return requests.get(url).text
